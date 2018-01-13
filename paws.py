530e41079c4efdca9486da3770ece96f412f34b8
epicmindwarp <epicmindwarp@gmail.com>
2018-01-13 16:17:42 +0000

    Added paws config file

---
+++ paws.py
@@ -0,0 +1,21 @@
+username = "Username"
+password = "Password"
+client_id = ""
+client_secret = ""
+
+#COMMENT THEN PARENT COMMENT
+trade_complete_reply = """
+Trade has been confirmed by /u/{0}.
+
+/u/{0} and /u/{1} have gained +1 trade
+"""
+
+new_scam_post_reply = """
+Hi! 
+
+We are sorry for your loss. 
+
+Your report has been filed by the Moderators of /r/DogeMarket. The Moderators will check your thread and ban the user if needed.
+
+Thanks for your patience.
+"""
